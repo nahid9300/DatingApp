886435cb1d189b11fa5da7cda9645450d126167b
Nahidul Islam Prince <princeneo58@hotmail.com>
2020-08-29 13:54:18 +0600

    Error handling done

--- DatingApp-SPA/src/app/register/register.component.ts
+++ DatingApp-SPA/src/app/register/register.component.ts
@@ -19,7 +19,7 @@ export class RegisterComponent implements OnInit {
     this.authService.register(this.model).subscribe(()=>{
       console.log('registration successful')
     }, error=>{
-      console.log('registration failed');
+      console.log(error);
     }
     );
   }
